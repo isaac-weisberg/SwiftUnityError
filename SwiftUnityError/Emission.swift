8d213af0d4f9147e53fe88163952dddbea4894d6
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-11-12 21:51:38 +0700

    Add UnityError and emissions

---
+++ SwiftUnityError/Emission.swift
@@ -0,0 +1,11 @@
+//
+//  Emission.swift
+//  SwiftUnityError
+//
+//  Created by Isaac Weisberg on 11/12/18.
+//  Copyright © 2018 Isaac Weisberg. All rights reserved.
+//
+
+public extension UnityError {
+    public typealias Emission = String
+}
---
+++ SwiftUnityError/UnityError.swift
@@ -0,0 +1,11 @@
+//
+//  UnityError.swift
+//  SwiftUnityError
+//
+//  Created by Isaac Weisberg on 11/12/18.
+//  Copyright © 2018 Isaac Weisberg. All rights reserved.
+//
+
+public protocol UnityError {
+    public var unity: Emission { get }
+}
